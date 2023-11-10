0ca81187d523841c9a64025ac1fee5f9860788b0
Antonio Albuquerque <114232542+antonioz2022@users.noreply.github.com>
2023-11-10 10:13:09 -0300

    Add files via upload

--- index.py
+++ index.py
@@ -1,4 +1,38 @@
-def agrupar_livros_categorias(arquivo_nome):
+def main():
+    #buscar("books.txt", "romance")
+    #print(gastosTotais("books.txt"))
+    #agruparLivros("books.txt")
+    opção = -1
+    while opção != 0:
+        printMenu()
+        opção = int(input())
+        if(opção == 1):
+            adicionarLivro("books.txt")
+        elif(opção == 2):
+            removerLivro("books.txt")
+        elif(opção == 3):
+            agruparLivros("books.txt")
+        elif(opção == 4):
+            print("Gastos totais:",gastosTotais("books.txt"), "R$")
+        elif(opção == 5):
+            genero = input("Digite o genero que você deseja buscar:")
+            buscar("books.txt", genero)
+        elif(opção == 0):
+            print("Desligando...")
+        else:
+            print("Opção não reconhecida!")
+
+def printMenu():
+    print("           Menu")
+    print("1 - Adicione um livro")
+    print("2 - Remova um livro")
+    print("3 - Agrupe livros em gêneros")
+    print("4 - Gastos totais")
+    print("5 - Buscar livros por gênero")
+    print("0 - Parar o programa")
+
+
+def agruparLivros(arquivo_nome):
     dictionary_categorias = {}
     arquivo = open(arquivo_nome, "r")
     for f in arquivo:
@@ -16,24 +50,24 @@ def agrupar_livros_categorias(arquivo_nome):
     arquivo.close()
 
 
-def adicionar_livro(arquivo_nome):
+def adicionarLivro(arquivo_nome):
     print("Hello World")
 
 
-def remover_livro(arquivo_nome):
+def removerLivro(arquivo_nome):
     print("Hello World")
 
 
-def busca(arquivo_nome, genero):
+def buscar(arquivo_nome, genero):
     arquivo = open(arquivo_nome, "r")
     for f in arquivo:
         arquivo_split = f.strip().split(",")
-        if arquivo_split[3] == genero:
+        if arquivo_split[3].lower() == genero.lower():
             print(arquivo_split[0])
     arquivo.close()
 
 
-def gastos_totais(arquivo_nome):
+def gastosTotais(arquivo_nome):
     arquivo = open(arquivo_nome, "r")
     soma = 0
     for f in arquivo:
@@ -42,12 +76,6 @@ def gastos_totais(arquivo_nome):
     arquivo.close()
     return soma
 
-
-def main():
-    busca("books.txt", "romance")
-    print(gastos_totais("books.txt"))
-    agrupar_livros_categorias("books.txt")
-
-
 if __name__ == "__main__":
     main()
+
